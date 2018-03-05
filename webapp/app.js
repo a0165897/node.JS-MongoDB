7f9b1f2135cdb2a0a96927643cf95c09dae4913b
Lee <943662850@qq.com>
2018-03-05 18:24:33 +0800

    7.2.8

--- webapp/app.js
+++ webapp/app.js
@@ -16,10 +16,32 @@
 var http    = require('http'),
     express = require('express'),
     app     = express(),
-    server  = http.createServer(app);
+    server  = http.createServer(app),
+    env     = process.env,
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    errorHandler = require("express-error-handler"),
+    logger  = require('morgan');
 //------------- END MODULE SCOPE VARIABLES ----------------
 
 //============= BEGIN SERVER CONFIG =======================
+app.use(bodyParser());
+app.use(methodOverride());
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(logger());
+    app.use(errorHandler({
+        dumpExceptions : true,
+        showStack      : true
+    }));
+    // console.log("setted dev");
+}
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(errorHandler());
+    // console.log("setted prd");
+}
+
 app.get('/',function (request , response) {
     response.send('Hello Express !');
 });
@@ -31,3 +53,4 @@ console.log(
     'Express server listening on port %d in %s mode',
     server.address().port , app.settings.env
 );
+// console.log(process.env.NODE_ENV);
